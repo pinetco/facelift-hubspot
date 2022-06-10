557691d4f3eaa031e440b459714b1dde41494617
jaypinetco <jay@pinetco.com>
2022-06-10 11:05:29 +0530

    form css updated and aspect ratio css added

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,79 @@ $('.one-col-slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
 });
+/* Three Col Slider */
+$('.three-col-slider').slick({
+    dots: true,
+    arrows: true,
+    autoplaySpeed: 5000,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    swipeToSlide: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+/* Four Col Slider */
+$('.four-col-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    swipeToSlide: true,
+    autoplay: true,
+    infinite: false,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
 
 /*  FAQ Logic */
 $(".accordion-question").click(function(){
