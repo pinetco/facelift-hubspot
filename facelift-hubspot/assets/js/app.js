bb8f4cabd452cadd0d4d77d43873c3ca6304e453
jaypinetco <jay@pinetco.com>
2022-07-19 15:40:57 +0530

    Navigation Css Updated

--- facelift-hubspot/assets/js/app.js
+++ facelift-hubspot/assets/js/app.js
@@ -201,29 +201,6 @@ $("[data-toggle='video']").click(function () {
 $("[data-close]").click(function () {
   $(this).parents(".modal").fadeOut();
 });
-$(function () {
-  // DOM ready
-  // If a link has a dropdown, add sub menu toggle.
-  $('nav ul li a:not(:only-child)').click(function (e) {
-    $(this).siblings('.nav-dropdown').toggle(); // Close one dropdown when selecting another
-
-    $('.nav-dropdown').not($(this).siblings()).hide();
-    e.stopPropagation();
-  }); // Clicking away from dropdown will remove the dropdown class
-
-  $('html').click(function () {
-    $('.nav-dropdown').hide();
-  }); // Toggle open and close nav styles on click
-
-  $('#nav-toggle').click(function () {
-    $('nav ul').slideToggle();
-  }); // Hamburger to X toggle
-
-  $('#nav-toggle').on('click', function () {
-    this.classList.toggle('active');
-  });
-}); // end DOM ready
-
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
 
@@ -232,9 +209,6 @@ $(window).scroll(function () {
   } else {
     $(".header").removeClass("white-header");
   }
-});
-$(".menu-toggle").click(function () {
-  $(".facelift-navigation-menu").slideToggle();
 });
 $(".hs-item-has-children").append("<span class='block submenu-toggle'><span class='block w-full h-full submenu-arrow'></span></span>");
 $(".submenu-toggle").click(function () {
